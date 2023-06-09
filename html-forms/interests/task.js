b269e642992233b5ea6df47700aaf8070a15f553
Sergey <example@gmail.com>
2023-06-09 22:26:21 +0300

    fix

--- html-forms/interests/task.js
+++ html-forms/interests/task.js
@@ -1,55 +1,19 @@
 const interestCheck = document.querySelectorAll(".interest__check");
 
-// function funcChange(event) {
-//   const checkbox = event.target;
-//   console.log(checkbox);
-
-//   const childCheckboxes = checkbox
-//     .closest(".interest")
-//     .querySelectorAll(".interest__check");
-
-//   childCheckboxes.forEach((childCheckbox) => {
-//     if (childCheckbox.checked !== checkbox.checked) {
-//       childCheckbox.checked = checkbox.checked;
-//     }
-//   });
-// }
-// interestCheck.forEach((checkbox) => {
-//     checkbox.addEventListener("change", funcChange);
-//   });
-
-//Расширенный вариант
-
 function funcChange(event) {
   const checkbox = event.target;
+  console.log(checkbox);
 
-  changeChildCheckboxes(checkbox);
-  changeParentCheckboxes(checkbox);
-}
-
-function changeChildCheckboxes(check) {
-  const childCheckboxes = check
+  const childCheckboxes = checkbox
     .closest(".interest")
     .querySelectorAll(".interest__check");
 
   childCheckboxes.forEach((childCheckbox) => {
-    if (childCheckbox.checked !== check.checked) {
-      childCheckbox.checked = check.checked;
-      changeChildCheckboxes(check);
+    if (childCheckbox.checked !== checkbox.checked) {
+      childCheckbox.checked = checkbox.checked;
     }
   });
 }
-
-function changeParentCheckboxes(check) {
-  const parentEl = check.closest(".interest").parentNode.closest(".interest");
-
-  if (!parentEl) {
-    return;
-  }
-
-  const parentCheckbox = parentEl.querySelector(".interest__check");
-}
-
 interestCheck.forEach((checkbox) => {
   checkbox.addEventListener("change", funcChange);
 });
